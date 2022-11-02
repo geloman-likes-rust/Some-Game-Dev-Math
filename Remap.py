54001ccf78ab66001ab82787280e584a03292137
Mark Angelo Torres <tmarkangelo96@gmail.com>
2022-11-02 09:59:58 +0800

    Lerp and Remap

---
+++ Remap.py
@@ -0,0 +1,9 @@
+def Lerp(a, b, t):
+    return (1.0f - t) * a + b * t
+
+def InvLerp(a, b, v):
+    return (v - a) / (b - a)
+
+def Remap(iMin, iMax, oMin, oMax, v):
+    t = InvLerp(iMin, iMax, v)
+        return Lerp(oMin, oMax, t)
